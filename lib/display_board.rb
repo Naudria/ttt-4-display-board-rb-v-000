34ab14f5f8260c1edfe8079e248b80547e9e5525
Rima Jean <rima.jean@gmail.com>
2018-01-19 16:37:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,73 @@
 # out the current state.
 def display_board(board)
   board = [" "," "," "," "," "," "," "," "," "]
-  puts "#{row[0]}"
-  puts "#{row[1]}"
-  puts "#{row[2]}"
-  puts "#{row[3]}"
-  puts "#{row[4]}"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board(board)
+  board = [" "," "," "," ","X"," "," "," "," "]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board(board)
+  board = ["O"," "," "," "," "," "," "," "," "]
+  puts " O |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board(board)
+  board = ["O"," "," "," ","X"," "," "," "," "]
+  puts " O |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board(board)
+  board = ["X","X","X"," "," "," "," "," "," "]
+  puts " X | X | X "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board(board)
+  board = [" "," "," "," "," "," ","O","O","O"]
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts " O | O | O "
+end
+def display_board(board)
+  board = ["X"," "," "," ","X"," "," "," ","X"]
+  puts " X |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   | X "
+end
+def display_board(board)
+  board = [" "," ","O"," ","O"," ","O"," "," "]
+  puts "   |   | O "
+  puts "-----------"
+  puts "   | O |   "
+  puts "-----------"
+  puts " O |   |   "
+end
+def display_board(board)
+  board = ["X","O","X","O","X","X","O","X","O"]
+  puts " X | O | X "
+  puts "-----------"
+  puts " O | X | X "
+  puts "-----------"
+  puts " O | X | O "
 end
