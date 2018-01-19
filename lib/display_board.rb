adcea0d6cfba03b07244d74c3073acd93730d57c
Rima Jean <rima.jean@gmail.com>
2018-01-19 16:34:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,11 @@
 # out the current state.
 def display_board(board)
   board = [" "," "," "," "," "," "," "," "," "]
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts "#{row[0]}"
+  puts "#{row[1]}"
+  puts "#{row[2]}"
+  puts "#{row[3]}"
+  puts "#{row[4]}"
 end
 def display_board(board)
   board = [" "," "," "," ","X"," "," "," "," "]
